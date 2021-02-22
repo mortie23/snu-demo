d9d5a2f08df8bb892a3dc34d414a503c5d62dd71
Christopher Mortimer <christopher@mortimer.xyz>
2021-02-22 15:35:21 +1100

    change command to run using porject file

--- snu.ps1
+++ snu.ps1
@@ -6,4 +6,26 @@
 .Description
 Single CLI command
 #>
-snu -source:"jdbc:teradata://192.168.190.128" -su:mortch -sp:mortch -ss:PRD_ADS_HWD_RETAILBANKING_DB -target:"jdbc:sxv4:C:\data\retailbanking"  -create -insert -cleanaction:skip
+
+<#
+Not working currently
+Build finished with cleansing actions taken
+str.channel.ChannelException: A database error occurred.
+Cause:
+        str.database.DatabaseException: An error occured while commiting changes to the database connection
+        Cause:
+                java.sql.SQLException: Key violation on add row.
+                SQLState: 23000
+
+Channel has abnormally ended.  Please see errors for details
+
+*** Caught Channel exception ***
+
+str.channel.ChannelException: A database error occurred.
+Cause:
+str.database.DatabaseException: An error occured while commiting changes to the database connection
+Cause:
+java.sql.SQLException: Key violation on add row.
+#>
+#snu -source:"jdbc:teradata://192.168.190.128" -su:mortch -sp:mortch -ss:PRD_ADS_HWD_RETAILBANKING_DB -target:"jdbc:sxv4:C:\data\retailbanking-snu"  -create -insert 
+snu -create -insert -sp:mortch .\retailbanking.xml
